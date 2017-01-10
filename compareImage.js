5b5513fd0966c4f1fbb6f641590b8112acb705ab
imsukmin <imsukmin@gmail.com>
2017-01-11 04:33:24 +0900

    change mod targetImagePath and compareImagePath

--- compareImage.js
+++ compareImage.js
@@ -2,6 +2,8 @@ var fs = require('fs'),
     gm = require('gm');
 
 module.exports = function(targetImagePath, compareImagePath, resolve){
+    fs.chmodSync(targetImagePath, 777);
+    fs.chmodSync(compareImagePath, 777);
     gm.compare(targetImagePath, compareImagePath, function (err, isEqual, equality, raw) {
         if (err) throw err;
         var isDoingWork = equality<0.08;
